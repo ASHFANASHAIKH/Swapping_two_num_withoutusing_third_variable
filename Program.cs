d08a56949b2c77a50f02643fbea09858fb833527
ashfana <ashfana@DESKTOP-NPR6MTA>
2021-11-13 13:01:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_two_num_withoutusing_third_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 6;
+            Console.WriteLine("Before swap a=" + a + " b= " + b);
+            a = a * b;//a=30(5*6)
+            b = a / b;//b=5(30/6)
+            a = a / b;//a=6(30/5)
+            Console.WriteLine("after swap a=" + a + " b= " + b);
+        }
+    }
+}
